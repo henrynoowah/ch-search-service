45b0c4a6f06fdf3eada2fb10358b435df3e2767c
조하운 <79410456+henrynoowah@users.noreply.github.com>
2022-11-23 14:36:14 +0900

    feat: jest

---
+++ src/search.test.ts
@@ -0,0 +1,24 @@
+import { azsearch } from "./_azSearch/azsearch";
+import { Idx, SearchServiceConfig } from "./_azSearch/azsearch.type";
+
+let params: SearchServiceConfig;
+
+params = {
+  options: {
+    searchTerm: "Mina Plastic Surgery",
+    select: ["Id"],
+  },
+  az_search_endpoint: process.env.AZ_SEARCH_ENDPOINT,
+  az_search_key: process.env.AZ_SEARCH_KEY,
+  idx: Idx.hospitals,
+  stage: "int",
+};
+
+test("Search Mina Plastic Surgerya", async () => {
+  try {
+    const { results } = await azsearch(params);
+    expect(results[0].Id).toBe("10a038b4-a11a-469a-bb57-bf4e1c764325");
+  } catch (e) {
+    expect(e).toMatch("Error");
+  }
+});
